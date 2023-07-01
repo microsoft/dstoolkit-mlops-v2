5246e0c4118293725028bd22995503365dafddbc
ritesh.modi <rimod@microsoft.com>
2023-07-01 13:48:33 +0100

    refactor

--- mlops/common/deployment/provision_deployment.py
+++ mlops/common/deployment/provision_deployment.py
@@ -29,6 +29,7 @@ parser.add_argument("--deployment_base_image", type=str, help="Azureml inference
 parser.add_argument("--deployment_conda_path", type=str, help="conda file path for inferencing image")
 parser.add_argument("--score_dir", type=str, help="name of directory with score file")
 parser.add_argument("--score_file_name", type=str, help="score file name")
+parser.add_argument("--build_id", type=str, help="build responsbile for deployment")
 parser.add_argument("--is_batch", type=str, help="batch deployment")
 args = parser.parse_args()
 
@@ -40,6 +41,7 @@ deployment_base_image = args.deployment_base_image
 deployment_conda_path = args.deployment_conda_path
 score_dir =  args.score_dir
 score_file_name = args.score_file_name
+build_id = args.build_id
 batch = args.is_batch
 
 print(f"Endpoint name: {endpoint_name}")
@@ -73,6 +75,7 @@ blue_deployment = ManagedOnlineDeployment(
     ),
     instance_type=deployment_vm_size,
     instance_count=1,
+    tags={"build_id": build_id},
 )
 
 ml_client.online_deployments.begin_create_or_update(blue_deployment).result()
--- mlops/common/deployment/provision_endpoint.py
+++ mlops/common/deployment/provision_endpoint.py
@@ -17,12 +17,14 @@ parser.add_argument("--subscription_id", type=str, help="Azure subscription id")
 parser.add_argument("--resource_group_name", type=str, help="Azure Machine learning resource group")
 parser.add_argument("--workspace_name", type=str, help="Azure Machine learning Workspace name")
 parser.add_argument("--endpoint_name", type=str, help="Azureml realtime endpoint name")
+parser.add_argument("--build_id", type=str, help="build responsbile for deployment")
 parser.add_argument("--is_batch", type=str, help="batch endpoint provisioning")
 args = parser.parse_args()
 
 
 endpoint_name = args.endpoint_name
 batch = args.is_batch
+build_id = args.build_id
 
 print(f"Endpoint name: {endpoint_name}")
 
@@ -35,7 +37,7 @@ if batch == "False":
         name=endpoint_name,
         description="An online endpoint serving an MLflow model for the diabetes classification task",
         auth_mode="key",
-        tags={"foo": "bar"},
+        tags={"build_id": build_id},
     )
 
     ml_client.online_endpoints.begin_create_or_update(endpoint=endpoint).result()
