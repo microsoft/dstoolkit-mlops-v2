4519a4eeb02ba054238fe95227c1f0905ece6b0b
Eddie Carrasco Jr <eduardo.lalo.carrasco.jr@gmail.com>
2023-08-29 10:29:37 -0700

    Update variables.tf

--- infra/terraform/public_workspace/variables.tf
+++ infra/terraform/public_workspace/variables.tf
@@ -1,18 +1,18 @@
 variable "subscription_id" {
   type    = string
-  default = "310ab569-9762-484e-8ce9-a650803297ea"
+  default = ""
 }
 variable "tenant_id" {
   type    = string
-  default = "16b3c013-d300-468d-ac64-7eda0820b6d3"
+  default = ""
 }
 variable "client_id" {
   type    = string
-  default = "78c775fe-e914-408d-8105-2ab2b2f4b775"
+  default = ""
 }
 variable "client_secret" {
   type    = string
-  default = "brU8Q~C2n1C2X9BD9y1busRqQaEV65AN2lOPZcdj"
+  default = ""
 }
 
 ##############################
@@ -20,20 +20,20 @@ variable "client_secret" {
 ##############################
 variable "basename" {
   type    = string
-  default = "mlopstest"
+  default = ""
 }
 
 variable "project_code" {
   type    = string
-  default = "v2"
+  default = ""
 }
 
 variable "version_num" {
   type    = string
-  default = "001"
+  default = ""
 }
 
 variable "location" {
   type    = string
-  default = "westus2"
+  default = ""
 }
