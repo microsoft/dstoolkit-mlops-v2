d2be50aab3bc970af2f070aceaf5dd30655f2c5a
Eddie Carrasco Jr <eduardo.lalo.carrasco.jr@gmail.com>
2023-08-29 11:29:00 -0700

    Add files via upload

--- infra/terraform/public_workspace/provider.tf
+++ infra/terraform/public_workspace/provider.tf
@@ -1,4 +1,5 @@
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {
-  }
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
 }
