f7ce4fb1781d203765c949f2b9fd8e536d73cb1e
Eddie Carrasco Jr <eduardo.lalo.carrasco.jr@gmail.com>
2023-08-31 08:43:29 -0700

    Update provider.tf

--- infra/terraform/public_workspace/provider.tf
+++ infra/terraform/public_workspace/provider.tf
@@ -2,4 +2,9 @@
 provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
