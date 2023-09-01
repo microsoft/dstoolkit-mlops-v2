b10e421ec54cceb6a435b264daa01881f03b0b7b
Eddie Carrasco Jr <eduardo.lalo.carrasco.jr@gmail.com>
2023-09-01 07:30:45 -0700

    Add files via upload

--- infra/terraform/public_workspace/variables.tf
+++ infra/terraform/public_workspace/variables.tf
@@ -18,6 +18,13 @@ variable "client_secret" {
 ##############################
 ## Resource Group Variables
 ##############################
+variable "rg_name" {
+  type    = string
+    validation {
+        condition = (length(var.rg_name) <= 90 && length(var.rg_name) > 2 && can(regex("[-\\w\\._\\(\\)]+", var.rg_name)) )
+        error_message = "Resource group name may only contain alphanumeric characters, dash, underscores, parentheses and periods."
+    }
+}
 variable "basename" {
   type    = string
   default = ""
