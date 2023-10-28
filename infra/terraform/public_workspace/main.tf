3a141e88a7aba5035af6512372e95b4ada1fe666
lferdinand <lferdinand@microsoft.com>
2023-10-28 05:31:00 -0700

    refer to resource group name using var.rg_name

--- infra/terraform/public_workspace/main.tf
+++ infra/terraform/public_workspace/main.tf
@@ -8,14 +8,14 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_application_insights" "aml_appins" {
   name                = "aml-ai-${var.basename}-${var.project_code}-${var.version_num}"
   location            = var.location
-  resource_group_name = rg.name
+  resource_group_name = var.rg_name
   application_type    = "web"
 }
 
 resource "azurerm_key_vault" "akv" {
   name                = "akvaml${var.basename}${var.project_code}${var.version_num}"
   location            = var.location
-  resource_group_name = rg.name
+  resource_group_name = var.rg_name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "premium"
 }
@@ -23,7 +23,7 @@ resource "azurerm_key_vault" "akv" {
 resource "azurerm_storage_account" "stacc" {
   name                     = lower("staccaml${var.basename}${var.project_code}${var.version_num}")
   location                 = var.location
-  resource_group_name      = rg.name
+  resource_group_name      = var.rg_name
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
@@ -31,7 +31,7 @@ resource "azurerm_storage_account" "stacc" {
 resource "azurerm_container_registry" "acr" {
   name                          = "cr${var.basename}${var.project_code}${var.version_num}"
   location                      = var.location
-  resource_group_name           = rg.name
+  resource_group_name           = var.rg_name
   sku                           = "Basic"
   admin_enabled                 = true
   }
@@ -39,7 +39,7 @@ resource "azurerm_container_registry" "acr" {
   resource "azurerm_machine_learning_workspace" "adl_mlw" {
   name                          = "mlw-${var.basename}-${var.project_code}-${var.version_num}"
   location                      = var.location
-  resource_group_name           = rg.name
+  resource_group_name           = var.rg_name
   application_insights_id       = azurerm_application_insights.aml_appins.id
   key_vault_id                  = azurerm_key_vault.akv.id
   storage_account_id            = azurerm_storage_account.stacc.id
