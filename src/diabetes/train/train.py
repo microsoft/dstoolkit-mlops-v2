cb9ffc3b49409728af4d3a871ca94b68ece5aa7e
deepakas <deepakas@users.noreply.github.com>
2023-11-27 12:02:37 -0800

    Update train.py
    Updated back to SVM

--- src/diabetes/train/train.py
+++ src/diabetes/train/train.py
@@ -77,10 +77,8 @@ def train_model(trainX, trainy):
     with mlflow.start_run() as run:
         # model = LinearRegression().fit(trainX, trainy)
 
-        model = GradientBoostingClassifier(
-        n_estimators=100, learning_rate=0.1
-    )
-        # model = SVC(kernel='linear', C=1.0)
+       # model = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1)
+        model = SVC(kernel='linear', C=1.0)
         model.fit(trainX, trainy)
 
         # print(model.score(trainX, trainy))
