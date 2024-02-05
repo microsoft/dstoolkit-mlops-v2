c8898b975c8da26d06fd305612d508fdf7c731e8
lferdinand <lferdinand@microsoft.com>
2024-02-05 06:35:56 -0700

    examine inputs for mlflow.register_model()

--- src/london_src/register/register.py
+++ src/london_src/register/register.py
@@ -25,6 +25,8 @@ def main(model_metadata, model_name, score_report, build_reference):
         cod = score_data["cod"]
         mse = score_data["mse"]
         coff = score_data["coff"]
+        
+        print(run_uri + " " + model_name)
 
         model_version = mlflow.register_model(run_uri, model_name)
 
