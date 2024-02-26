2a2edbc5fd7a5851bc636764729eadcfe0823430
michelle <michelleluo@microsoft.com>
2024-02-25 21:25:37 -0500

    data pipeline setup initially

--- mlops/common/register_data_asset.py
+++ mlops/common/register_data_asset.py
@@ -22,18 +22,22 @@ from mlops.common.config_utils import MLOpsConfig
 
 config = MLOpsConfig()
 
-ml_client = MLClient(
-    DefaultAzureCredential(),
-    config.aml_config["subscription_id"],
-    config.aml_config["resource_group_name"],
-    config.aml_config["workspace_name"]
-)
+#ml_client = MLClient(
+#    DefaultAzureCredential(),
+#    config.aml_config["subscription_id"],
+#    config.aml_config["resource_group_name"],
+#    config.aml_config["workspace_name"]
+#)
 
 parser = argparse.ArgumentParser("register data assets")
 
+parser.add_argument("--subscription_id", type=str, help="Azure subscription id", required=True)
+parser.add_argument("--resource_group_name", type=str, help="Azure Machine learning resource group", required=True)
+parser.add_argument("--workspace_name", type=str, help="Azure Machine learning Workspace name", required=True)
 parser.add_argument("--data_purpose", type=str, help="data to be registered identified by purpose", required=True)
 parser.add_argument("--data_config_path", type=str, help="data config file path", required=True)
 parser.add_argument("--environment_name", type=str, help="environment name (e.g. dev, test, prod)", required=True)
+parser.add_argument("--api_version", type=str, help="API version to use (optional)")
 
 args = parser.parse_args()
 
@@ -41,7 +45,13 @@ data_purpose = args.data_purpose
 data_config_path = args.data_config_path
 environment_name = args.environment_name
 
-config_file = open(data_config_path)
+ml_client = MLClient(
+    DefaultAzureCredential(), args.subscription_id, args.resource_group_name, args.workspace_name,
+    api_version=args.api_version
+)
+
+config_file = open("mlops/nyc_taxi/configs/data_config.json")
+#config_file = open(data_config_path)
 data_config = json.load(config_file)
 
 for elem in data_config['datasets']:
