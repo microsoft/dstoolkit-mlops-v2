04fc6159390c4bd343f6ce91db385fbad58f7a9f
michelle <michelleluo@microsoft.com>
2024-02-29 10:25:31 -0500

    data registration updates

--- mlops/common/register_data_asset.py
+++ mlops/common/register_data_asset.py
@@ -2,11 +2,8 @@
 This module is designed to register data assets in an Azure Machine Learning environment.
 
 It utilizes the Azure AI MLClient from the Azure Machine Learning SDK to interact with Azure resources.
-The module parses command-line arguments to receive necessary details like subscription ID, resource group name,
-workspace name, data purpose, data configuration path, and environment name.
 
-The script reads a configuration file to identify and register datasets in Azure Machine Learning based on their purpose
-and the specified environment (development, test, production, etc.). It supports operations like creating or updating
+The script reads a configuration file to identify and register datasets in Azure Machine Learning. It supports operations like creating or updating
 data assets and retrieving the latest version of these assets.
 """
 
@@ -20,64 +17,50 @@ from azure.ai.ml.constants import AssetTypes
 
 from mlops.common.config_utils import MLOpsConfig
 
-config = MLOpsConfig()
+def main():
+    """Register all datasets from the config file."""
+    config = MLOpsConfig()
 
-ml_client = MLClient(
-    DefaultAzureCredential(),
-    config.aml_config["subscription_id"],
-    config.aml_config["resource_group_name"],
-    config.aml_config["workspace_name"],
-)
+    ml_client = MLClient(
+        DefaultAzureCredential(),
+        config.aml_config["subscription_id"],
+        config.aml_config["resource_group_name"],
+        config.aml_config["workspace_name"],
+    )
 
-parser = argparse.ArgumentParser("register data assets")
+    parser = argparse.ArgumentParser("register data assets")
 
-parser.add_argument(
-    "--data_purpose",
-    type=str,
-    help="data to be registered identified by purpose",
-    required=True,
-)
-parser.add_argument(
-    "--data_config_path", type=str, help="data config file path", required=True
-)
-parser.add_argument(
-    "--environment_name",
-    type=str,
-    help="environment name (e.g. dev, test, prod)",
-    required=True,
-)
+    parser.add_argument(
+        "--data_config_path", type=str, help="data config file path", required=True
+    )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-data_purpose = args.data_purpose
-data_config_path = args.data_config_path
-environment_name = args.environment_name
+    data_config_path = args.data_config_path
 
-config_file = open(data_config_path)
-data_config = json.load(config_file)
+    config_file = open(data_config_path)
+    data_config = json.load(config_file)
 
-for elem in data_config["datasets"]:
-    if "DATA_PURPOSE" in elem and "ENV_NAME" in elem:
-        if (
-            data_purpose == elem["DATA_PURPOSE"]
-            and environment_name == elem["ENV_NAME"]
-        ):
-            data_path = elem["DATA_PATH"]
-            dataset_desc = elem["DATASET_DESC"]
-            dataset_name = elem["DATASET_NAME"]
+    for elem in data_config["datasets"]:
+        data_path = elem["DATA_PATH"]
+        dataset_desc = elem["DATASET_DESC"]
+        dataset_name = elem["DATASET_NAME"]
 
-            aml_dataset = Data(
-                path=data_path,
-                type=AssetTypes.URI_FOLDER,
-                description=dataset_desc,
-                name=dataset_name,
-            )
+        aml_dataset = Data(
+            path=data_path,
+            type=AssetTypes.URI_FOLDER,
+            description=dataset_desc,
+            name=dataset_name,
+        )
 
-            ml_client.data.create_or_update(aml_dataset)
+        ml_client.data.create_or_update(aml_dataset)
 
-            aml_dataset_unlabeled = ml_client.data.get(
-                name=dataset_name, label="latest"
-            )
+        aml_dataset_unlabeled = ml_client.data.get(
+            name=dataset_name, label="latest"
+        )
 
-            print(aml_dataset_unlabeled.latest_version)
-            print(aml_dataset_unlabeled.id)
+        print(aml_dataset_unlabeled.latest_version)
+        print(aml_dataset_unlabeled.id)
+
+if __name__ == "__main__":
+    main()
