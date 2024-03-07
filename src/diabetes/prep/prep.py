1f74c2eb0e4c8bb485c08b144154205526283123
deepakas <deepak.subhramanian@gmail.com>
2024-03-07 15:15:41 -0800

    Debugging diabetes code

--- src/diabetes/prep/prep.py
+++ src/diabetes/prep/prep.py
@@ -1,26 +1,16 @@
-"""
-This module is designed to preprocess taxi data for the Azure Machine Learning NYC Taxi tutorial.
-
-It includes functions for reading raw taxi data (both green and yellow taxi data),
-cleaning it, and transforming it into a format suitable for machine learning modeling.
-The module focuses on selecting useful columns, renaming them as per tutorial specifications,
-and concatenating the green and yellow taxi data into a single dataset.
-"""
-
 import argparse
 from pathlib import Path
+from typing_extensions import Concatenate
+from uuid import uuid4
+from datetime import datetime
 import os
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import pickle
 
 
 def main(raw_data, prep_data):
-    """
-    Read existing csv files and invoke preprocessing step.
-
-    Parameters:
-      raw_data (str): a folder to read csv files
-      prep_data (str): a folder for preprocessed data
-    """
     print("hello training world...")
 
     lines = [
@@ -38,7 +28,7 @@ def main(raw_data, prep_data):
     df_list = []
     for filename in arr:
         print("reading file: %s ..." % filename)
-        with open(os.path.join(raw_data, filename), "r"):
+        with open(os.path.join(raw_data, filename), "r") as handle:
             input_df = pd.read_csv((Path(raw_data) / filename))
             df_list.append(input_df)
 
@@ -51,14 +41,10 @@ def main(raw_data, prep_data):
 
 
 def data_prep(data):
-    """
-    Define useful columns needed for the Azure Machine Learning NYC Taxi tutorial.
+    # Define useful columns needed for the Azure Machine Learning NYC Taxi tutorial
+    # Pregnancies	Glucose	BloodPressure	SkinThickness	Insulin	BMI	DiabetesPedigreeFunction	Age	Outcome
 
-    Output columns are Pregnancies	Glucose	BloodPressure	SkinThickness	Insulin	BMI	DiabetesPedigreeFunction	Age	Outcome
 
-    Parameters:
-      data (pandas.DataFrame): raw data
-    """
     useful_columns = str(
         [
             "pregnancies",
@@ -86,45 +72,43 @@ def data_prep(data):
             "DiabetesPedigreeFunction": "diabetespedigreefunction",
             "Age": "age",
             "Outcome": "outcome",
+           
         }
     ).replace(",", ";")
 
-    print("data_columns: " + data_columns)
 
-    data_clean = cleansedata(data, data_columns, useful_columns)
+
+    print("data_columns: " + data_columns)
+   
+
+    data_clean = cleanseData(data, data_columns, useful_columns)
+
 
     # # Append yellow data to green data
     # combined_df = pd.concat([green_data_clean, yellow_data_clean], ignore_index=True)
     # #combined_df = green_data_clean.append(yellow_data_clean, ignore_index=True)
-
+    
     combined_df = data_clean
     combined_df.reset_index(inplace=True, drop=True)
 
-    # output_green = data_clean.to_csv(
-    #     os.path.join(prep_data, "clean_data.csv")
-    # )
+    output_green = data_clean.to_csv(
+        os.path.join(prep_data, "clean_data.csv")
+    )
     # output_yellow = yellow_data_clean.to_csv(
     #     os.path.join(prep_data, "yellow_prep_data.csv")
     # )
-    # merged_data =
-    combined_df.to_csv(os.path.join(prep_data, "merged_data.csv"))
+    merged_data = combined_df.to_csv(os.path.join(prep_data, "merged_data.csv"))
+
+
 
     print("Finish")
+
 
 # These functions ensure that null data is removed from the dataset,
 # which will help increase machine learning model accuracy.
 
 
 def get_dict(dict_str):
-    """
-    Ensure that null data is removed from the dataset to increase machine learning model accuracy.
-
-    Parameters:
-      dict_str (Dictionary): a string with separated elements
-
-    Returns:
-      Dictionary: an updated dictionary
-    """
     pairs = dict_str.strip("{}").split(";")
     new_dict = {}
     for pair in pairs:
@@ -134,18 +118,7 @@ def get_dict(dict_str):
     return new_dict
 
 
-def cleansedata(data, columns, useful_columns):
-    """
-    Clean dataset removing NA values.
-
-    Parameters:
-      data (pandas.DataFrame): initial data
-      columns (str): a list of columns in initial dataset
-      useful_columns (str): columns to retain
-
-    Returns:
-      DataFrame: an updated data set
-    """
+def cleanseData(data, columns, useful_columns):
     useful_columns = [
         s.strip().strip("'") for s in useful_columns.strip("[]").split(";")
     ]
