5246b88dad031375a29dc517c5b9960153a2cd79
deepakas <deepak.subhramanian@gmail.com>
2024-03-07 15:39:06 -0800

    Debugging diabetes code

--- src/diabetes/train/train.py
+++ src/diabetes/train/train.py
@@ -1,13 +1,3 @@
-"""
-This module is designed to train a machine learning model.
-
-The module performs the following key steps:
-1. Reading and combining data from specified training data files.
-2. Splitting the combined data into training and testing datasets.
-3. Training a Linear Regression model using the training dataset.
-4. Using MLflow for logging and tracking experiments.
-5. Saving the trained model and its metadata to specified paths.
-"""
 import argparse
 from pathlib import Path
 import os
@@ -20,18 +10,10 @@ import json
 
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import classification_report
-
+from sklearn.metrics import confusion_matrix
+from sklearn.svm import SVC 
 
 def main(training_data, test_data, model_output, model_metadata):
-    """
-    Read training data, split data and initiate training.
-
-    Parameters:
-      training_data (str): training data folder
-      test_data (str): test data folder
-      model_output (str): a folder to store model files
-      model_metadata (str): a file to store information about thr model
-    """
     print("Hello training world...")
 
     lines = [
@@ -51,35 +33,22 @@ def main(training_data, test_data, model_output, model_metadata):
     df_list = []
     for filename in arr:
         print("reading file: %s ..." % filename)
-        with open(os.path.join(training_data, filename), "r"):
+        with open(os.path.join(training_data, filename), "r") as handle:
             input_df = pd.read_csv((Path(training_data) / filename))
             df_list.append(input_df)
 
     train_data = df_list[0]
     print(train_data.columns)
 
-    trainx, testx, trainy, testy = split(train_data)
-    write_test_data(testx, testy)
-    train_model(trainx, trainy)
+    trainX, testX, trainy, testy = split(train_data)
+    write_test_data(testX, testy)
+    train_model(trainX, trainy)
 
 
 def split(train_data):
-    """
-    Split the input data into training and testing sets.
-
-    Parameters:
-    train_data (DataFrame): The input data.
-
-    Returns:
-    trainX (DataFrame): The training data.
-    testX (DataFrame): The testing data.
-    trainy (Series): The training labels.
-    testy (Series): The testing labels.
-    """
     # Split the data into input(X) and output(y)
-
-    y = train_data["cost"]
-    x = train_data[
+    y = train_data["outcome"]
+    X = train_data[
         [
             "pregnancies",
             "glucose",
@@ -93,44 +62,36 @@ def split(train_data):
     ]
 
     # Split the data into train and test sets
-    trainx, testx, trainy, testy = train_test_split(
-        x, y, test_size=0.3, random_state=42
+    trainX, testX, trainy, testy = train_test_split(
+        X, y, test_size=0.3, random_state=42
     )
-    print(trainx.shape)
-    print(trainx.columns)
+    print(trainX.shape)
+    print(trainX.columns)
 
-    return trainx, testx, trainy, testy
+    return trainX, testX, trainy, testy
 
 
-def train_model(trainx, trainy):
-    """
-    Train a Linear Regression model and save the model and its metadata.
-
-    Parameters:
-    trainX (DataFrame): The training data.
-    trainy (Series): The training labels.
-
-    Returns:
-    None
-    """
+def train_model(trainX, trainy):
     mlflow.autolog()
     # Train a Linear Regression Model with the train set
     with mlflow.start_run() as run:
         # model = LinearRegression().fit(trainX, trainy)
 
-        model = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1)
-        # model = xgb.XGBClassifier(objective="binary:logistic", random_state=42)
+        model = GradientBoostingClassifier(
+        n_estimators=100, learning_rate=0.1
+    )
         # model = SVC(kernel='linear', C=1.0)
-        model.fit(trainx, trainy)
+        # model.fit(trainX, trainy)
 
         # print(model.score(trainX, trainy))
 
-        y_pred = model.predict(trainx)
+        y_pred = model.predict(trainX)
 
-        print(y_pred)
+        print (y_pred)
         print(classification_report(trainy, y_pred))
         # cm = confusion_matrix(trainX, y_pred)
         # print(cm)
+
 
         # Output the model, metadata and test data
         run_id = mlflow.active_run().info.run_id
@@ -142,20 +103,10 @@ def train_model(trainx, trainy):
         pickle.dump(model, open((Path(args.model_output) / "model.sav"), "wb"))
 
 
-def write_test_data(testx, testy):
-    """
-    Write the testing data to a CSV file.
-
-    Parameters:
-    testX (DataFrame): The testing data.
-    testy (Series): The testing labels.
-
-    Returns:
-    None
-    """
-    testx["outcome"] = testy
-    print(testx.shape)
-    testx.to_csv((Path(args.test_data) / "test_data.csv"))
+def write_test_data(testX, testy):
+    testX["outcome"] = testy
+    print(testX.shape)
+    testX.to_csv((Path(args.test_data) / "test_data.csv"))
 
 
 if __name__ == "__main__":
