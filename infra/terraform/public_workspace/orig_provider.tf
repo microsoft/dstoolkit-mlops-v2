aae057d845e2add1dc3a1e0c35147bfca21a927c
lferdinand <lferdinand@microsoft.com>
2024-07-03 15:51:04 -0700

    modify provider file

--- infra/terraform/public_workspace/orig_provider.tf
+++
@@ -1,9 +0,0 @@
-# Configure the Microsoft Azure Provider
-terraform {
-  backend "azurerm" {}
-}
-provider "azurerm" {
-  skip_provider_registration = "true"
-  features {}
-  
-}
--- infra/terraform/public_workspace/provider.tf
+++ infra/terraform/public_workspace/provider.tf
@@ -1,36 +1,14 @@
-variable "oidc_token" {}
-variable "oidc_token_file_path" {}
-variable "oidc_request_token" {}
-variable "oidc_request_url" {}
-
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.7.0"
-    }
-  }
-}
-
 # Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
+  terraform {
+    backend "azurerm" {      
+      use_oidc             = true  # Can also be set via `ARM_USE_OIDC` environment variable.}
 
-  subscription_id = env.SUBSCRIPTION_ID
-  client_id       = env.ARM_CLIENT_ID
-  use_oidc        = true
-
-  # for GitHub Actions
-  oidc_request_token = var.oidc_request_token
-  oidc_request_url   = var.oidc_request_url
-
-  # for other generic OIDC providers, providing token directly
-  oidc_token = var.oidc_token
-
-  # for other generic OIDC providers, reading token from a file
-  oidc_token_file_path = var.oidc_token_file_path
-
-  tenant_id = env.ARM_TENANT_ID
+    } 
 }
+  provider "azurerm" {
+    
+    use_oidc = true
+    skip_provider_registration = "true"
+    features {}
+    
+  }
