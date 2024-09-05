3156a0e4e093896f6017901e9dff4ce7d05efb70
Kristin Ottofy <krottofy@microsoft.com>
2024-09-05 20:57:19 +0000

    allow dockerfile environment

--- mlops/common/get_environment.py
+++ mlops/common/get_environment.py
@@ -7,18 +7,20 @@ authenticate with Azure, define environment specifications, and perform environm
 """
 
 from azure.ai.ml import MLClient
+from azure.ai.ml.entities import BuildContext, Environment
 from azure.identity import DefaultAzureCredential
-from azure.ai.ml.entities import Environment
 
 
 def get_environment(
     subscription_id: str,
     resource_group_name: str,
     workspace_name: str,
-    env_base_image_name: str,
-    conda_path: str,
     environment_name: str,
     description: str = "Azure ML environment",
+    env_base_image_name: str = None,
+    conda_path: str = None,
+    docker_context_path: str = None,
+    dockerfile_path: str = None,
 ):
     """
     Create or update Azure ML environment and return a reference to it.
@@ -31,7 +33,8 @@ def get_environment(
       conda_path (str): a path to a conda file with additional packages to install
       environment_name (str): a name of the environment
       description (str): a description of the environment
-
+      docker_context_path (str): The local or remote path to the the docker build context directory
+      dockerfile_path (str): The path to the dockerfile relative to root of docker build context directory
     Returns:
       Environment: an object that represents the environment
     """
@@ -43,16 +46,24 @@ def get_environment(
             resource_group_name=resource_group_name,
             workspace_name=workspace_name,
         )
-        env_docker_conda = Environment(
-            image=env_base_image_name,
-            conda_file=conda_path,
-            name=environment_name,
-            description=description,
-        )
+        if conda_path:
+            env_docker_conda = Environment(
+                image=env_base_image_name,
+                conda_file=conda_path,
+                name=environment_name,
+                description=description,
+            )
+        else:
+            env_docker_conda = Environment(
+                build=BuildContext(
+                    path=docker_context_path, dockerfile_path=dockerfile_path
+                ),
+                name=environment_name,
+                description=description,
+            )
         environment = client.environments.create_or_update(env_docker_conda)
         print(f"Environment {environment_name} has been created or updated.")
         return environment
-
     except Exception as ex:
         print(
             "Oops! invalid credentials or error while creating ML environment.. Try again...",
