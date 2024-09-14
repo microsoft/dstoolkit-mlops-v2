17e4e51c7f9f2d8dca8429b0bf1876a82389620e
STIJEPOVIC <nina.stijepovic-ext@alstomgroup.com>
2024-09-14 16:22:16 +0000

    vars

--- infra/terraform/public_workspace/output.tf
+++ infra/terraform/public_workspace/output.tf
@@ -1,23 +1,27 @@
-output "client_id" {
-  value = azurerm_user_assigned_identity.mlops_identity.client_id
-}
-
-output "tenant_id" {
-  value = azurerm_user_assigned_identity.mlops_identity.tenant_id
-}
-
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
-
 output "storage_account_name" {
   value = azurerm_storage_account.stacc.name
+}
+
+output "container_registry_name" {
+  value = azurerm_container_registry.acr.name
+}
+
+output "appinsights_name" {
+  value = azurerm_application_insights.aml_appins.name
 }
 
 output "keyvault_name" {
   value = azurerm_key_vault.akv.name
 }
 
-output "container_registry_name" {
-  value = azurerm_container_registry.acr.name
+output "workspace_name" {
+  value = azurerm_machine_learning_workspace.adl_mlw.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "location" {
+  value = var.location
 }
